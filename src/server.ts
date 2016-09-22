c61052f21d46d191e5fdc1fe7412430c9376bea3
mnemonic101 <mnemonic@codemonic.net>
2016-09-22 14:47:11 +0200

    Cleanup

--- src/server.ts
+++ src/server.ts
@@ -3,25 +3,6 @@ import {AutoWired, Provides} from "typescript-mvc";
 
 @AutoWired
 @Provides(WebServer)
-class Server extends WebServer {
-
-  public onBeforeServerStart(): void {
-
-    this.codeShouldBeMovedIntoFramework();
-  }
-
-  // TODO: move this into the mvc framework soon
-  private codeShouldBeMovedIntoFramework(): void {
-    // TODO: add system default request handlers, e.g. error
-    this.context.framework.router.addRequestHandler("/", (request: any, response: any, next: any) => {
-      this.context.logger.log("Unhandled request:", request.url);
-      next();
-    });
-
-    // TODO: add processing of application level adapter registrations for ILogger, IFactory, IDataStore etc.
-    //
-
-  }
-}
+class Server extends WebServer { }
 
 Server.start();

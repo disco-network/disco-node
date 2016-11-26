46e0ee5403c6178ce395afca4ffe207060b4f7cb
Datokrat <pa.rei@gmx.de>
2016-11-26 18:28:39 +0100

    working seeds

--- source/lib/system/adapter/data/seed.ts
+++ source/lib/system/adapter/data/seed.ts
@@ -1,4 +1,6 @@
 import * as rdfstore from "rdfstore";
+import * as path from "path";
+import * as fs from "fs";
 
 const storeUri = "http://disco-network.org/resource/";
 
@@ -10,60 +12,19 @@ export function seed() {
       const seeder = new Seeder(store);
       seeder.begin();
 
-      seeder.insertMany(seeder.insertCulture, [{
-        "Id": "1",
-        "Key": "ce30d475-b11e-46ac-8de9-3f3b89a89344",
-        "Modified": "2014-03-26T20:03:30.797",
-        "Code": "en-US",
-        "Name": "english (UK)",
-      },
-      {
-        "Id": "2",
-        "Key": "7b5e1699-c6f8-472b-976a-bb37ce517219",
-        "Modified": "2014-02-18T22:56:15.003",
-        "Code": "de-DE",
-        "Name": "german (Germany)",
-      }]);
+      const dbSeedFolderPath = "../../../../../db/seed";
 
-      seeder.insertPostType({
-        "Id": "1",
-        "Key": "86ca262f-0d31-4e61-bd79-b6de8b784fab",
-        "Modified": "2014-02-18T22:56:15.09",
-        "Description": null,
-        "DescriptionName": "Topic",
-        "DescriptionKey": "a3952f8c-ecc5-4aaf-8891-a25952ae523d",
-        "DescriptionModified": "2014-02-18T22:56:15.09",
-      });
+      const cultures = require(`${dbSeedFolderPath}/01_Cultures.json`).value;
+      const content = require(`${dbSeedFolderPath}/02_Content.json`).value;
+      const descriptors = require(`${dbSeedFolderPath}/03_Descriptors.json`).value;
+      const postTypes = require(`${dbSeedFolderPath}/10_PostTypes.json`).value;
+      const posts = require(`${dbSeedFolderPath}/11_Posts.json`).value;
 
-      seeder.insertMany<DPost>(seeder.insertPost, [{
-        "Title": null,
-        "Text": "Innen, Recht, Demokratie, Sicherheit",
-        "Key": "004c55c4-d71c-4307-ab57-36d907b12584",
-        "Modified": "2014-02-18T22:56:15.127",
-        "CultureId": "2",
-        "ContentKey": "da922338-6d8a-4cc0-addc-6670201ad9e1",
-        "ContentModified": "2014-02-18T22:56:15.127",
-        "PostTypeId": "1",
-      },
-      {
-        "Title": null,
-        "Text": "Au\u00dfen, Internationales, Frieden",
-        "CultureId": "2",
-        "Key": "bf38a895-264f-40d9-81f8-63f224dacc46",
-        "Modified": "2014-02-18T22:56:15.103",
-        "ContentKey": "bff86dae-58d0-4463-b5e3-7332311ea0b6",
-        "ContentModified": "2014-02-18T22:56:15.12",
-        "PostTypeId": "1",
-      }, {
-        "Key": "6099ee26-97a2-4890-85c8-e1c87c63d834",
-        "Modified": "2014-02-18T22:56:15.127",
-        "PostTypeId": "1",
-        "ContentKey": "80616591-0e9f-41d3-9b73-7adef946edaf",
-        "ContentModified": "2014-02-18T22:56:15.127",
-        "Title": null,
-        "Text": "Konzepte f\u00fcr Basisdemokratie",
-        "CultureId": "2",
-      }]);
+      seeder.insertMany(seeder.insertCulture, cultures);
+      seeder.insertMany(seeder.insertContent, content);
+      seeder.insertMany(seeder.insertDescriptor, descriptors);
+      seeder.insertMany(seeder.insertPostType, postTypes);
+      seeder.insertMany(seeder.insertPost, posts);
 
       seeder.save(storeUri, () => store["close"]());
     }
@@ -104,21 +65,23 @@ export class Seeder {
 
   public insertPost(data: DPost) {
     if (!this.readyForSeeding) throw new Error("not ready for seeding, call begin().");
-    const postUri = this.uri(this.genEntityUri("Posts", data.Id));
-    const contentUri = this.genEntityUri("Content", data.ContentId);
-    const postTypeUri = this.genEntityUri("PostTypes", data.PostTypeId);
+    const postUri = this.genEntityUri("Posts", data.Id);
 
-    this.triple(postUri, this.resolve("rdf:type"), this.resolve("disco:Post"));
     this.applyEntityBaseProperties(postUri, "disco:Post", data);
-    this.triple(postUri, this.resolve("disco:content"), this.uri(contentUri));
-    this.triple(postUri, this.resolve("disco:postType"), this.uri(postTypeUri));
+    this.navProperty(postUri, "disco:content", "Content", data.ContentId);
+    this.navProperty(postUri, "disco:postType", "PostTypes", data.PostTypeId);
   }
 
   public insertContent(data: DContent) {
     if (!this.readyForSeeding) throw new Error("not ready for seeding, call begin().");
-    const contentUri = this.uri(this.genEntityUri("Content", data.Id));
+    const contentUri = this.genEntityUri("Content", data.Id);
 
-    this.applyEntityBaseProperties(contentUri, data);
+    this.applyEntityBaseProperties(contentUri, "disco:Content", data);
+
+    if (data.Title)
+      this.property(contentUri, "disco:title", data.Title);
+    this.property(contentUri, "disco:text", data.Text);
+    this.navProperty(contentUri, "disco:culture", "Cultures", data.CultureId);
   }
 
   public insertCulture(data: DCulture) {
@@ -126,46 +89,45 @@ export class Seeder {
 
     const cultureUri = this.genEntityUri("Cultures", data.Id);
 
-    this.triple(this.uri(cultureUri), this.resolve("rdf:type"), this.resolve("disco:Culture"));
-    this.triple(this.uri(cultureUri), this.resolve("disco:id"), this.literal(data.Id));
-    this.triple(this.uri(cultureUri), this.resolve("disco:key"), this.literal(data.Key));
-    this.triple(this.uri(cultureUri), this.resolve("disco:foo"), this.literal("Hi"));
-    this.triple(this.uri(cultureUri), this.resolve("disco:modified"), this.literal(data.Modified));
-    this.triple(this.uri(cultureUri), this.resolve("disco:code"), this.literal(data.Code));
-    this.triple(this.uri(cultureUri), this.resolve("disco:name"), this.literal(data.Name));
+    this.applyEntityBaseProperties(cultureUri, "disco:Culture", data);
+
+    this.property(cultureUri, "disco:code", data.Code);
+    this.property(cultureUri, "disco:name", data.Name);
   }
 
   public insertPostType(data: DPostType) {
-    const postTypeUri = this.genEntityUri("PostTypes", data.Id);
-    const descriptionIdentity = this.genNextIdentity("Descriptors");
+    const uri = this.genEntityUri("PostTypes", data.Id);
 
-    this.triple(this.uri(postTypeUri), this.resolve("rdf:type"), this.resolve("disco:PostType"));
-    this.triple(this.uri(postTypeUri), this.resolve("disco:id"), this.literal(data.Id));
-    this.triple(this.uri(postTypeUri), this.resolve("disco:key"), this.literal(data.Key));
-    this.triple(this.uri(postTypeUri), this.resolve("disco:foo"), this.literal("Hi"));
-    this.triple(this.uri(postTypeUri), this.resolve("disco:modified"), this.literal(data.Modified));
-    this.triple(this.uri(postTypeUri), this.resolve("disco:description"), this.uri(descriptionIdentity.uri));
+    this.applyEntityBaseProperties(uri, "disco:PostType", data);
+    this.navProperty(uri, "disco:description", "Descriptors", data.DescriptionId);
+  }
 
-    this.triple(this.uri(descriptionIdentity.uri), this.resolve("rdf:type"), this.resolve("disco:Descriptor"));
-    this.triple(this.uri(descriptionIdentity.uri), this.resolve("disco:id"), this.literal(descriptionIdentity.id));
-    this.triple(this.uri(descriptionIdentity.uri), this.resolve("disco:key"), this.literal(data.DescriptionKey));
-    this.triple(this.uri(descriptionIdentity.uri), this.resolve("disco:foo"), this.literal("Hi"));
-    this.triple(
-      this.uri(descriptionIdentity.uri), this.resolve("disco:modified"), this.literal(data.DescriptionModified));
-    this.triple(this.uri(descriptionIdentity.uri), this.resolve("disco:name"), this.literal(data.DescriptionName));
-    if (data.Description !== null)
-      this.triple(this.uri(descriptionIdentity.uri), this.resolve("disco:description"), this.literal(data.Description));
+  public insertDescriptor(data: DDescriptor) {
+    const uri = this.genEntityUri("Descriptors", data.Id);
+
+    this.applyEntityBaseProperties(uri, "disco:Descriptor", data);
+    this.property(uri, "disco:name", data.Name);
+    this.property(uri, "disco:description", data.Description);
+    this.navProperty(uri, "disco:culture", "Cultures", data.CultureId);
+  }
+
+  public navProperty(uri, property: string, entitySet: string, id: string) {
+    this.triple(uri, this.resolve(property), this.genEntityUri(entitySet, id));
+  }
+
+  public property(uri, property: string, literal: string) {
+    this.triple(uri, this.resolve(property), this.literal(literal));
   }
 
   public applyEntityBaseProperties(uri, rdfType: string, data: DEntity) {
-    this.triple(uri, this.resolve("rdf:type"), this.literal(rdfType));
+    this.triple(uri, this.resolve("rdf:type"), this.resolve(rdfType));
     this.triple(uri, this.resolve("disco:id"), this.literal(data.Id));
     this.triple(uri, this.resolve("disco:key"), this.literal(data.Key));
     this.triple(uri, this.resolve("disco:modified"), this.literal(data.Modified));
   }
 
   private genEntityUri(entitySet: string, entityId: string) {
-    return `http://disco-network.org/resource/${entitySet}/${entityId}`;
+    return this.uri(`http://disco-network.org/resource/${entitySet}/${entityId}`);
   }
 
   private triple(s, p, o) {
